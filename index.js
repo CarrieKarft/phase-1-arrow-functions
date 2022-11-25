94a8c4af5f230d425233c9b644875b1ca2a5c4c5
Carrie Kraft <carriekarft@gmail.com>
2022-11-24 18:13:32 -0800

    commit tree b5ef5030a38ad0370d6352904d1f4d147f8da952

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+    let num1 = 2000
+    let num2 = 100
+    return num1 / num2;
+}
+
+const square = number => number * number;
+
+const add = (number1, number2) => number1 + number2;
